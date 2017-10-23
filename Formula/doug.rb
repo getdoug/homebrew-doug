7fd46babb93e5a736c7ade1683045c4fc829afd9
Steve Dignam <steve@dignam.xyz>
2017-10-22 23:56:13 -0400

    Initial commit

---
+++ Formula/doug.rb
@@ -0,0 +1,20 @@
+class Doug < Formula
+  desc "Command line tool for time tracking"
+  homepage "https://github.com/getdoug/doug"
+  url "https://github.com/getdoug/doug/archive/1.0.0.tar.gz"
+  sha256 ""
+  head "https://github.com/getdoug/doug.git"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+
+    bin.install "target/release/doug"
+  end
+
+  test do
+    system "#{bin}/doug", "cancel"
+  end
+
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-doug
+> a homebrew formula for `doug`
+
+## Install
+
+```
+brew install getdoug/doug/doug
+```
